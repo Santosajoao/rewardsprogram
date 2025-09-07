49712f7e4a19ecb8a132451fe9d3578cbb8e0dcd
Joao Araujo <joaosantos162003@gmail.com>
2025-09-07 19:37:34 -0300

    feat: adicionar suporte ao PWA com configuração do Service Worker

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+   reactStrictMode: true,
 };
 
+// Importa o wrapper do PWA
+const withPWA = require("@ducanh2912/next-pwa").default({
+  dest: "public", // Onde os arquivos do Service Worker serão gerados (dentro da pasta public)
+  register: true, // Registra automaticamente o Service Worker
+  skipWaiting: true, // Força o novo Service Worker a ativar imediatamente
+  disable: process.env.NODE_ENV === "development", // Desativa o PWA em modo de desenvolvimento (para evitar dores de cabeça com cache)
+});
+module.exports = withPWA(nextConfig);
 export default nextConfig;
